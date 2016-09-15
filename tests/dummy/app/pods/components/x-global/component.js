642327a4cde1d378ac1018ed7151dd40ce9a6d74
Steven Glanzer <sglanzer@ciena.com>
2016-09-15 00:32:39 -0400

    Linting

--- tests/dummy/app/pods/components/x-global/component.js
+++ tests/dummy/app/pods/components/x-global/component.js
@@ -1,7 +1,7 @@
-import Ember from 'ember';
-import layout from './template';
+import Ember from 'ember'
+import layout from './template'
 
 export default Ember.Component.extend({
   hook: 'pods_x-global',
   layout
-});
+})
